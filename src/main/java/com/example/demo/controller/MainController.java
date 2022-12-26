46c02effbe07b0bb544ea7b40c536c1327e73ce8
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-12-26 12:28:30 +0900

    (refactor)

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -60,7 +60,7 @@ public class MainController {
     }
 
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/alluser")
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
