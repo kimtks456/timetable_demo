5eebac6c44d5256bd2012019ef610a6719e781d9
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-12-30 23:55:07 +0900

    (feat) CORS 설정에 모든 HTTP methods 추가

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://gdsc-timetable.vercel.app", "http://localhost:5173");
+                .allowedOrigins("https://gdsc-timetable.vercel.app", "http://localhost:5173")
+                .allowedMethods("*");
     }
 }
